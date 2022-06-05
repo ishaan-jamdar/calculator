0a58564ea6025daeeeef938296bdcf59ee91042d
Ishaan Jamdar <ishaan.jamdar@gmail.com>
2022-06-04 20:24:43 -0400

    Added styles, buttons, and screen functionality

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+const nums = document.querySelectorAll('.num');
+const operators = document.querySelectorAll('.op');
+const decimal = document.querySelector('#decimal');
+const clearBtn = document.querySelector('#clear');
+const deleteBtn = document.querySelector('#delete');
+const prevScreen = document.querySelector('.history');
+const mainScreen = document.querySelector('.current');
+
+for (let num of nums) {
+  num.addEventListener('click', () => mainScreen.textContent += num.textContent);
+}
+
+for (let op of operators) {
+  op.addEventListener('click', () => {
+
+  });
+}
+
+decimal.addEventListener('click', () => {
+  if (!mainScreen.textContent.includes('.')) {
+    mainScreen.textContent += '.';
+  }
+});
+
+clearBtn.addEventListener('click', () => {
+  prevScreen.textContent = '';
+  mainScreen.textContent = '';
+});
+
+deleteBtn.addEventListener('click', () => mainScreen.textContent = mainScreen.textContent.slice(0, -1));
+
 function add(a, b) {
   return a + b;
 }
@@ -12,4 +43,16 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(a, b, op) {
+  if (op === '+') {
+    return add(a, b);
+  } else if (op === '-') {
+    return subtract(a, b);
+  } else if (op === 'x') {
+    return multiply(a, b);
+  } else if (op === '÷') {
+    return divide(a, b);
+  }
 }

77869f07ab135e82092cd5d900f501750217a12a
Ishaan Jamdar <ishaan.jamdar@gmail.com>
2022-06-04 21:01:29 -0400

    Finished most functionality

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+let lastEqual = false;
+
 const nums = document.querySelectorAll('.num');
 const operators = document.querySelectorAll('.op');
-const decimal = document.querySelector('#decimal');
+const decimalBtn = document.querySelector('#decimal');
 const clearBtn = document.querySelector('#clear');
 const deleteBtn = document.querySelector('#delete');
+const equalsBtn = document.querySelector('#equals');
 const prevScreen = document.querySelector('.history');
 const mainScreen = document.querySelector('.current');
 
@@ -23,7 +26,7 @@ for (let op of operators) {
   });
 }
 
-decimal.addEventListener('click', () => {
+decimalBtn.addEventListener('click', () => {
   if (!mainScreen.textContent.includes('.')) {
     mainScreen.textContent += '.';
   }
@@ -35,6 +38,15 @@ clearBtn.addEventListener('click', () => {
 });
 
 deleteBtn.addEventListener('click', () => mainScreen.textContent = mainScreen.textContent.slice(0, -1));
+
+equalsBtn.addEventListener('click', () => {
+  if (prevScreen.textContent) {
+    prevNum = Number(prevScreen.textContent.slice(0, -2));
+    newNum = operate(prevNum, Number(mainScreen.textContent), prevScreen.textContent.slice(-1));
+    prevScreen.textContent = '';
+    mainScreen.textContent = newNum;
+  }
+});
 
 function add(a, b) {
   return a + b;
